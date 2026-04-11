502d770b8e2843834d3b7f60cae081a4f34d3944
sakshigirme03 <sakshi.k.girme@gmail.com>
2026-04-12 00:37:06 +0530

    Added method overloading example

--- Core Java/Methods.java
+++ Core Java/Methods.java
@@ -1,9 +1,38 @@
-public class Methods{
+//A method in Java is a block of code that performs a specific task and runs only when it is called.
 
-    public static int add(int a,int b){
+
+class Animal{
+    public void Cat(){
+        System.out.println("Meows");
+    }
+    public String Dog(int n){
+        if(n>=1000){
+            return "Breed";
+        }
+        return "Street dog ";
+    }
+
+}
+
+class Math{
+    public int Add(int a,int b){
         return a+b;
     }
+
+    public int Div(int a,int b){
+    return a/b;
+    }
+
+}
+
+public class Methods{
+
     public static void main(String []args){
-        System.out.println("addition of 2 no's 5 and 4 is "+add(5,4));
+        Animal obj= new Animal();
+        obj.Cat();
+        String Max= obj.Dog(2430);
+        System.out.println(Max);
+        Math obj1=new Math();
+        System.out.println("div of 4 and 2 :"+obj1.Div(4,2)+", Addition of 56,28: "+ obj1.Add(56,28));
     }
 }
