383a30e2b75b3b3562fddee15c8a7e70db28b68c
sakshigirme03 <sakshi.k.girme@gmail.com>
2026-04-11 22:53:42 +0530

    Initial commit - Java Framework Udemy

---
+++ Core Java/Methods.java
@@ -0,0 +1,9 @@
+public class Methods{
+
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String []args){
+        System.out.println("addition of 2 no's 5 and 4 is "+add(5,4));
+    }
+}
