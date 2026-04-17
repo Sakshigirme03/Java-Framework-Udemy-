a1f6c7073397c564d9fc0d2ac63b6f3b69bcf93d
sakshigirme03 <sakshi.k.girme@gmail.com>
2026-04-17 22:49:49 +0530

    Ex of String

---
+++ stringEg.java
@@ -0,0 +1,9 @@
+public class stringEg{
+    public static void main(String[]args){
+        String name="Sakshi";
+        String fullName= name + " Girme";
+        System.out.println("Name : "+name);
+        System.out.println("Character at index 1 is :"+ name.charAt(1));
+        System.out.println("Full name is: "+fullName);
+    }
+}
