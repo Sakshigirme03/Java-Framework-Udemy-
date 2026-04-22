07161376d57af0771f539334cb0bc5df894f1967
sakshigirme03 <sakshi.k.girme@gmail.com>
2026-04-22 22:58:50 +0530

    Encapsulation

--- encapsulation.java
+++ encapsulation.java
@@ -1,6 +1,7 @@
 public class encapsulation{
     public static void main(String[]args){
         System.out.println("Encapsulation");
-        System.out.println("Example  ");
+        System.out.println("Example   ");
+        System.out.println("sakk");
     }
 }
