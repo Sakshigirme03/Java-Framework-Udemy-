a09a9b07acf9e157393e1f41a0903c012e20fac1
sakshigirme03 <sakshi.k.girme@gmail.com>
2026-04-25 23:46:51 +0530

    Encapsulation example

--- encapsulation.java
+++ encapsulation.java
@@ -1,7 +1,19 @@
+class Human{
+    private int age;
+
+    public void setAge(int age){
+        this.age= age;
+    }
+
+    public int getAge(){
+        return age;
+    }
+}
+
 public class encapsulation{
     public static void main(String[]args){
-        System.out.println("Encapsulation");
-        System.out.println("Example   ");
-        System.out.println("sakk");
+        Human obj= new Human();
+        obj.setAge(25);
+        System.out.println(obj.getAge());
     }
 }
