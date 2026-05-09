9b081dd3a0b243704526097baa7796e8c6f2cf85
sakshigirme03 <sakshi.k.girme@gmail.com>
2026-05-09 23:12:47 +0530

    Naming Conventions

--- Core Java/namingConventions.java
+++ Core Java/namingConventions.java
@@ -1,6 +1,6 @@
 
 public class namingConventions{
     public static void main(String []args){
-        System.out.println(" Naming conventions ");
+        System.out.println("  Naming conventions ");
     }
 }
