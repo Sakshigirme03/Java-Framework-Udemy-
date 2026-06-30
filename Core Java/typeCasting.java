92f93d26a6761453e0b1d0598ec2daac3a83ef98
sakshigirme03 <sakshi.k.girme@gmail.com>
2026-06-30 21:32:45 +0530

    Typecasting in java

---
+++ Core Java/typeCasting.java
@@ -0,0 +1,25 @@
+class A{
+    public void show1(){
+        System.out.println("In A show");
+    }
+}
+
+class B extends A{
+    public void show2(){
+        System.out.println("In B show");
+    }
+}
+
+
+public class typeCasting {
+    public static void main(String [] args){
+
+        A obj = new B();
+        obj.show1(); // upcasting
+
+        B obj1 = (B) obj;
+        obj1.show2();
+
+
+    }
+}
